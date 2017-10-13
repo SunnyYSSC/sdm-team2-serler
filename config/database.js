860dd5755024050ce5fb6237dbecb959c3cb12ad
Sunny C <sunmaloto@outlook.com>
2017-10-13 16:25:44 +1300

    Changed database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://heroku_k7pjq9gv:herokukbq2867@ds117495.mlab.com:17495/heroku_k7pjq9gv' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://serlerdb:serler123456@ds117495.mlab.com:17495/heroku_k7pjq9gv' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
