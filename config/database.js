7591db755527dfad59aaa7ec5bc09f38ff5ac2b9
Sunny C <sunmaloto@outlook.com>
2017-10-13 14:07:32 +1300

    Changed gitignore file

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://heroku_k7pjq9gv:herokukbq2867@ds117495.mlab.com:17495/heroku_k7pjq9gv' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
